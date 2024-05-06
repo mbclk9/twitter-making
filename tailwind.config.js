9a768b6269a3cf98014307c834b4c24382526936
mbclk9 <mbclk94@gmail.com>
2024-05-06 22:27:50 +0300

    font değiştirildi

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         box:'rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px'
       },
       fontFamily:{
-        display:'Chirp'
+        display:'Poppins'
       }
     },
   },
